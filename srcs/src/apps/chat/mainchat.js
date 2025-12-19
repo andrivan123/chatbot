f2e5dbea203091314aa02e7a88df659cf73df3f3
andrii_ivanov2 <andrii_ivanov2@epam.com>
2025-12-19 12:42:55 +0100

    implement REST call

--- srcs/src/apps/chat/mainchat.js
+++ srcs/src/apps/chat/mainchat.js
@@ -6,19 +6,53 @@ const ai = new GoogleGenAI({apiKey: process.env.REACT_APP_GEMINI_API_KEY });
 
 
 export async function mainchat(text) {
-   let responseText =  Date.now();
+   let responseText = Date.now();
    console.log("mainchat()<",text)
 
-//    let response = await ai.models.generateContent({
-//     model: "gemini-2.5-flash",
-//     contents:text,
-//   });
-//   console.log(response.text);
-//   responseText = response.text
+   responseText = await usingLib(text)
 
-console.log("mainchat()>",responseText)
-return responseText;
+   console.log("mainchat()>",responseText)
+   return responseText;
 }
 
+async function usingLib(text){
+   let response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents:text,
+  });
+  console.log(response);
+  return response.text
+}
+
+function usingRest(text){
+   fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-goog-api-key': process.env.REACT_APP_GEMINI_API_KEY,
+      },
+      body: JSON.stringify({
+        contents: [
+          {
+            parts: [
+              {
+                text: text
+              }
+            ]
+          }
+        ]
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Response:', data);
+        // Handle the API response here
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        // Handle errors here
+      });
+
+}
 let q= "Explain how AI works in a few words"
 //mainchat(q);
